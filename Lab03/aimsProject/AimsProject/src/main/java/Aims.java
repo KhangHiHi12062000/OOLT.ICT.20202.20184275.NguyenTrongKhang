215e6172b191f18727de5417ff3d1f6573a0d9f8
KhangHiHi12062000 <khang.nt184275@sis.hust.edu.vn>
2021-03-19 13:49:18 +0700

    ex4

--- Lab03/aimsProject/AimsProject/src/main/java/Aims.java
+++ Lab03/aimsProject/AimsProject/src/main/java/Aims.java
@@ -6,12 +6,9 @@ public class Aims {
        Cart anOrder = new Cart();
        //Add DVD
         DigitalVideoDisc Div1 = new DigitalVideoDisc("The Lion King","Animation","Roger Allers",87, (float) 19.95);
-        anOrder.addDigitalVideoDisc(Div1);
         DigitalVideoDisc Div2 = new DigitalVideoDisc("Star Wars","Science","George Lucas",87, (float) 24.95);
-        anOrder.addDigitalVideoDisc(Div2);
         DigitalVideoDisc Div3 = new DigitalVideoDisc("Aladin","Animation","",0, (float) 18.99);
-        anOrder.addDigitalVideoDisc(Div3);
-        anOrder.removeDigitalVideoDisc(Div3);
+        anOrder.addDigitalVideoDisc(Div1,Div2,Div3);
         anOrder.totalCost();
         
     }
--- Lab03/aimsProject/AimsProject/src/main/java/Cart.java
+++ Lab03/aimsProject/AimsProject/src/main/java/Cart.java
@@ -3,13 +3,21 @@ public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
     private DigitalVideoDisc itemsOdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
     private static int qtyOdered = 0;
-    public void addDigitalVideoDisc(DigitalVideoDisc  Disc){
+    public void addDigitalVideoDisc(DigitalVideoDisc Disc){
             this.itemsOdered[qtyOdered] = Disc;
             this.itemsOdered[qtyOdered].print();
             qtyOdered++;
-            
+       }
+    public void addDigitalVideoDisc(DigitalVideoDisc [] dvdList){
+        if(dvdList.length>20){
+            System.out.println("You can add more than 20 DVDS");
+        }
+        else{
+        for(int i=0;i<=dvdList.length-1;i++){
+            addDigitalVideoDisc(DigitalVideoDisc dvd1,DigitalVideoDisc dvd2)
+        }
+        }
     }
-    
     public void removeDigitalVideoDisc(DigitalVideoDisc disc){
             for(int i=0;i<=qtyOdered-1;i++){
                 if(disc.getTitle().equals(this.itemsOdered[i].getTitle())){
@@ -24,12 +32,23 @@ public class Cart {
             }
            
     }
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1,DigitalVideoDisc dvd2){
+            this.itemsOdered[qtyOdered] = dvd1;
+            this.itemsOdered[qtyOdered].print();
+            this.itemsOdered[qtyOdered] = dvd2;
+            this.itemsOdered[qtyOdered].print();
+            qtyOdered += 2;
+    }
     public void totalCost(){
         float sum = 0;
         for(int i = 0 ; i <= qtyOdered - 1; i++){
             sum = sum + itemsOdered[i].getCost();
         }
         System.out.println("Total Cost : " + sum);
+    }
+
+    void addDigitalVideoDisc(DigitalVideoDisc Div1, DigitalVideoDisc Div2, DigitalVideoDisc Div3) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
             
 }
