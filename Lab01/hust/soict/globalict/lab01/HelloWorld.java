8145e0fc5e417d67b59ac481a53fc3ec2cd4f42e
Khang <KhangHiHi12062000>
2021-03-26 14:08:28 +0700

    after class lab05

---
+++ Lab01/hust/soict/globalict/lab01/HelloWorld.java
@@ -0,0 +1,6 @@
+package hust.soict.globalict.lab01;
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("HelloWorld");
+}
+}
---
+++ Lab01/hust/soict/globalict/lab01/SortArray.java
@@ -0,0 +1,32 @@
+package hust.soict.globalict.lab01;
+/*Ex6.5
+    
+    Sorting Array
+*/
+import java.util.Scanner;
+import java.util.Arrays;
+public class SortArray{
+public static void main(String[] args) {
+    
+    int sum = 0;    
+    double ave = 0;
+    int n = 0;
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter the size of Array: ");
+    n = input.nextInt();
+    int A[] = new int[n];
+    for(int i = 0;i<n;i++){
+        A[i] = input.nextInt();
+    }
+    Arrays.sort(A);
+    System.out.println("Sorted numeric array: " + Arrays.toString(A));
+    for(int i = 0;i<n;i++){
+        sum = sum + A[i];
+    }
+    ave = (double)sum/(double)n;
+    System.out.println("The sum of all elements of Array is : " + sum + " . ");
+    System.out.println("The average value of Array is : " + ave + " . ");
+
+    }
+}
