cc1c8e3b2550a916a6b145fc5077955d2b4a5997
KhangHiHi12062000 <khang.nt184275@sis.hust.edu.vn>
2021-04-10 23:28:24 +0700

    Add a feature for demonstrate

---
+++ Lab06/AimsProject/src/main/java/hust/soict/globalict/aims/Store/StoreTest.java
@@ -0,0 +1,22 @@
+package hust.soict.globalict.aims.Store;
+
+import hust.soict.globalict.aims.media.DigitalVideoDisc;
+import hust.soict.globalict.aims.store.Store;
+
+public class StoreTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", "Roger Allers", 18.99f);
+		
+		Store store = new Store();
+		store.addMedia(dvd1);
+		store.addMedia(dvd2);
+		store.addMedia(dvd3);
+		store.removeMedia(4);
+		store.printStore();
+	}
+
+}
